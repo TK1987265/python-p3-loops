4fc44c6d276c89515d24313fa2d3cce05db90708
Mohammad Khan <qcc2016@yahoo.com>
2024-06-25 16:57:31 -0400

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,34 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+
+        print(i)
+        i -= 1
+
+    print("Happy New Year!")
+   
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    # square_integers([1, 2, 3, 4, 5])
+    new_list = list()
+    for number in int_list:
+        inch_height = number * number
+        new_list.append(inch_height)
+    return new_list 
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        
+        if(i%3 ==0 and i%5==0):
+            print("FizzBuzz")
+        elif(i%3==0):
+            print("Fizz")
+        elif(i%5==0):
+            print("Buzz")
+        else:
+            print(i)
+    #  print(f"i is: {i}")
